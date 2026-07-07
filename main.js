44225a9045715bf12ff9d83cb8fd355bbb385d47
viktoriya-s <v.suprun1961@gmail.com>
2026-07-07 13:04:58 +0300

    add content

--- main.js
+++ main.js
@@ -6,16 +6,17 @@ const button = document.getElementById('button');
 
 admin.addEventListener('click', function () {
     modal.classList.add('open');
-
 });
 
 button.addEventListener('click', function () {
     const value = input.value;
     if (value === PAROL) {
-        window.location.href = "admin.html";
+        window.open("admin.html", "_blank");
+        // window.location.href = "admin.html";
         // или window.open("success.html", "_self");
     } else {
         alert("Неправильный пароль!");
     }
     input.value = '';
+    modal.classList.remove('open');
 });
