6ea0823298c5421e4315fcfc379991085cb35d41
viktoriya-s <v.suprun1961@gmail.com>
2026-07-07 12:22:40 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const modal = document.querySelector('.modal');
+const admin = document.getElementById('admin');
+const PAROL = '12345';
+const input = document.getElementById('input');
+const button = document.getElementById('button');
+
+admin.addEventListener('click', function () {
+    modal.classList.add('open');
+
+});
+
+button.addEventListener('click', function () {
+    const value = input.value;
+    if (value === PAROL) {
+        window.location.href = "admin.html";
+        // или window.open("success.html", "_self");
+    } else {
+        alert("Неправильный пароль!");
+    }
+    input.value = '';
+});
